ef387523570af7d48199aa676905020287b3d371
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-05-20 18:22:20 +0200

    random change for testing

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     counter += 1
     if (counter >= 10) {
-        counter = 0
+        counter += 0 - counter
     }
     basic.showNumber(counter)
 })
