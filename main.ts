27386cae87278b7ea6abcfc4086fbeb57b09fc9d
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-05-20 18:14:23 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter)
+basic.forever(function () {
+	
+})
